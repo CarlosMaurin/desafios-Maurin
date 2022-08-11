ec383c2060653dcb77026cb9f4b11a8df258d999
CarlosMaurin <carlosmaurin89@gmail.com>
2022-08-11 14:48:46 -0300

    desafio3-3

--- Desafio2/desafio2Articulos.js
+++ Desafio2/desafio2Articulos.js
@@ -1,13 +1,9 @@
 const fs = require("fs/promises");
 
-
-
-
 class Desafio2Articulos {
     constructor (ruta){
         this.ruta = ruta
     }
-
     async getAll(){
         try {
             const objs = await fs.readFile(this.ruta, "utf-8");
@@ -15,9 +11,7 @@ class Desafio2Articulos {
         } catch (error) {
             return [];
         }
-        
     };
-
     // async save(obj){
 
     //     try {
@@ -101,28 +95,31 @@ class Desafio2Articulos {
     //         console.log("error al eliminar los artículos");
     //     }
     // };
-
+    async nombreProd(){
+        try {
+            const objs = await this.getAll();
+            let nombres = objs.map(item => item.title);
+            return nombres;
+        } catch (error) {
+            console.log("error");            
+        }
+    }
     async randomProd(){
         try {
             const objs = await this.getAll();
             let random = Math.floor(Math.random() * objs.length);
             return objs[random];
         } catch (error) {
-            
+            console.log("error")
         }
     }
-
-
-
 }
-
 async function main(){
-
     const articulo = new Desafio2Articulos ("../Archivos/desafio2Articulos.json");
     // console.log("consulta de ruta");
     // console.log(articulo.ruta);
     // console.log("primera consulta");
-    console.log(await articulo.getAll());
+    // console.log(await articulo.getAll());
     // console.log("agregamos articulos");
     // console.log(await articulo.save({title: "anteojos ray ban", price: 15000, thumbnail: "https://http2.mlstatic.com/D_NQ_NP_713939-MLA49636746444_042022-O.webp"}));
     // console.log(await articulo.save({title: "anillo Bulgari", price: 3500, thumbnail: "https://http2.mlstatic.com/D_NQ_NP_732054-MLA49858468287_052022-O.webp"}));
@@ -139,13 +136,10 @@ async function main(){
     // console.log(await articulo.deleteById(3));
     // console.log("eliminamos todos los artículos");
     // console.log(await articulo.deleteAll());
+    console.log(await articulo.nombreProd())
     console.log(await articulo.randomProd());
-
 }
-
 main();
-
-
 module.exports = Desafio2Articulos;
 
 
