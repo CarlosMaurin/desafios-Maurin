7c4fe6361503db4d2d178d08dcaf425e137fddb5
CarlosMaurin <carlosmaurin89@gmail.com>
2022-08-23 21:01:49 -0300

    desafio5-parte1

---
+++ Desafio5/desafio5-HBS/desafio5hbs.js
@@ -0,0 +1,76 @@
+//------------------MODULOS---------------------------//
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+//-----------------SERVER------------------------------//
+
+const app = express();
+
+//----------------MIDDLEWARES--------------------------//
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname + "/public")))
+
+
+//-----motor de plantilla hbs-----------//
+
+
+app.engine("hbs", exphbs.engine({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get('views'), '/layouts'),
+    partialsDir: path.join(app.get('views'), '/partials'),
+    extname: 'hbs'
+}))
+app.set("views", path.join(__dirname + "/views"));
+app.set("view engine", "hbs")
+
+
+//----------------RUTAS-------------------------------//
+
+const DB_PRODUCTOS = [];
+
+app.get('/', (req, res)=>{
+    res.render('vista-form', {DB_PRODUCTOS});
+});
+
+app.get("/productos", (req, res)=>{
+    res.render("vista-hist", {DB_PRODUCTOS});
+})
+
+app.post('/productos', (req, res)=>{
+    DB_PRODUCTOS.push(req.body);
+    console.log(DB_PRODUCTOS);
+    res.redirect('/');
+});
+
+app.delete("/productos", (req, res)=>{
+    let arrayLength = DB_PRODUCTOS.length;
+    DB_PRODUCTOS.splice(0, arrayLength);
+    console.log(arrayLength);
+    // res.send(DB_PRODUCTOS.length = 0);
+    // res.send(DB_PRODUCTOS);
+    // console.log(DB_PRODUCTOS);
+    res.redirect('/');
+})
+
+
+
+app.get("*", (req, res)=>{
+    res.status(404).json({
+        code: 404,
+        msg: "not found"
+    })
+})
+
+
+
+//------------------LEVANTAR SERVIDOR-------------------//
+
+const PORT = 8080;
+const server = app.listen(PORT, ()=>{
+    console.log(`estamos escuchando el servidor http://localhost:${PORT}`)
+});
+server.on("error", error =>{
+    console.log(`Error en el servidor ${error}`)
+});
